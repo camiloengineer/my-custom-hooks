9cd7a826059d3d58a5478c32f351db7b88756c60
Camilo González <cgonzalezm1234@gmail.com>
2021-07-28 20:28:46 -0400

    First commit

---
+++ src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react'
+import '../02-useEffect/effects.css'
+
+export const FocusScreen = () => {
+
+    const inputRef = useRef();
+
+
+    const handleClick = () => {
+        inputRef.current.select();
+        console.log(inputRef)
+    }
+
+    return (
+        <div>
+            <h1>Focus Screen</h1>
+
+            <input
+            ref={ inputRef }
+            className="form-control"
+            placeholder="Su nombre"
+            type="text"
+            />
+
+            <button 
+            className="btn btn-outline-primary mt-5"
+            onClick={ handleClick }
+            >
+                Focus
+            </button>
+        </div>
+    )
+}
